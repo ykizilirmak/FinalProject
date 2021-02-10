b81c6391782bf109ea5165697909b6447e197c73
ykizilirmak <ykizilirmak93@gmail.com>
2021-02-10 15:35:00 +0300

    main

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -11,7 +11,7 @@ namespace Business.Abstract
         List<Product> GetAll();
         List<Product> GetAllByCategoryId(int id);
         List<Product> GetByUnitPrice(decimal min, decimal max);
-
+        
         List<ProductDetailDto> GetProductDetail();  
     }  
 }
